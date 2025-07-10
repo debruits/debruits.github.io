7841cbe03359fab8c48407428f55d91f65b74566
debruits <debruitsproject@gmail.com>
2025-07-10 01:40:23 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+const galeria = document.getElementById('gallery');
+
+const monitos = galeria.children;
+
+monitos.style.transition = 'all .5s';
+monitos.style.transform = 'rotate(25def)';
+monitos.style.transform = 'scale(1.4)';
