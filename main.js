ec957192872e92aca77715c24972985cb31d0ff7
debruits <debruitsproject@gmail.com>
2025-07-10 12:22:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-const galeria = document.getElementById('gallery');
+const monito = document.getElementById('gallery').children;
 
-const monitos = galeria.children;
+for(const item of monito){
 
-monitos.style.transition = 'all .5s';
-monitos.style.transform = 'rotate(25def)';
-monitos.style.transform = 'scale(1.4)';
+    item.addEventListener('mouseover', (e)=>{
+    e.target.style.transition ='all .2s';
+    e.target.style.transform ='scale(2) rotate(20deg)';
+    e.target.style.filter ='invert(100%)';
+    e.target.style.backgroundColor ='black';
+
+});
+
+}
+
+for(const items of monito){
+
+ items.addEventListener('mouseleave', (e)=>{
+    e.target.style.transition ='all .2s';
+    e.target.style.transform ='none';
+        e.target.style.filter ='invert(0)';
+    e.target.style.backgroundColor ='blue';
+
+
+
+});
+}
